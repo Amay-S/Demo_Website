9fb55f057ef707d0db5297e7fb9f97bd27f09811
Amay <amay.shah10@gmail.com>
2023-08-22 23:25:34 -0400

    commit

--- script/script.js
+++ script/script.js
@@ -1,17 +1,20 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let header = document.querySelector(".header");
+let homeSection = document.querySelector("#home");
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 };
 
-//scroll section
+// Scroll section
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
   let top = window.scrollY;
+  header.classList.toggle("sticky", window.scrollY > 30);
 
   section.forEach((sec, index) => {
     let offset = sec.offsetTop - 150;
@@ -32,43 +35,7 @@ window.onscroll = () => {
     }
   });
 
-  //stick navbar
-  let header = document.querySelector(".header");
-
-  header.classList.toggle("sticky", window.scrollY > 30);
-
-  // remove menu icon nav on click 
-
+  // Remove menu icon nav on click 
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
-
 };
-
-/*
-// dark mode 
-let modeToggle = document.getElementById('mode-toggle');
-let body = document.body;
-
-modeToggle.onclick = () => {
-  body.classList.toggle('dark-mode'); // Toggle dark mode styles
-  
-  // Toggle between moon and sun icons
-  if (modeToggle.classList.contains('bxs-moon')) {
-    modeToggle.classList.remove('bxs-moon');
-    modeToggle.classList.add('bx-sun');
-  } else {
-    modeToggle.classList.remove('bx-sun');
-    modeToggle.classList.add('bxs-moon');
-  }
-}
-*/
-
-
-ScrollReveal({ 
-  reset: true,
-  distance: '80px',
-  duration: 2000,
-  delay: 50
-});
-
-ScrollReveal().reveal('.home-content , .about-content, .portfolio-container, .slider', { origin: 'top'});
